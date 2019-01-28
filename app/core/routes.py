df44c0c51f2b36b3cb7659796b6dc1d28f5f4683
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-01-27 22:49:29 -0200

    Adcionado template(tag) formulário de paciente, Adicionado bulma css no template base

--- app/core/routes.py
+++ app/core/routes.py
@@ -5,3 +5,7 @@ from app.core import core
 @core.route('/index')
 def index():
     return render_template('index.html')
+
+@core.route('/paciente')
+def paciente():
+    return render_template('paciente.html')
